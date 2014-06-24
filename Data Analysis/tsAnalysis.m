80dad1915c03a5e56055c42ff8eaa4c1b4461b05
samuelyusunwang <samuelyusunwang@gmail.com>
2014-06-24 16:55:42 -0500

    test ts
    change

--- Data Analysis/tsAnalysis.m
+++ Data Analysis/tsAnalysis.m
@@ -42,17 +42,38 @@ tsc1 = resample(tsc, 1:0.5:24);
 hold on
 plot(tsc1.intersection1, '-xb', 'Displayname', 'Intersection 1');
 plot(tsc1.intersection2, '-.xm', 'Displayname', 'Intersection 2');
-plot(tsc1.intersection1, ':xr', 'Displayname', 'Intersection 3');
+plot(tsc1.intersection3, ':xr', 'Displayname', 'Intersection 3');
+legend('show', 'Location', 'Best')
+hold off
 
+% Add data sample
+tsc1 = addsampletocollection(tsc1, 'time', 3.25, 'intersection1', 5);
 
+% Delete data sample
+tsc1 = delsamplefromcollection(tsc1, 'index', find(isnan(tsc1.intersection2.Data)));
 
+% Add back data sample
+tsc1 = addsampletocollection(tsc1, 'time', 3.25, 'intersection1', 5);
 
+% Interpolate the missing values in tsc1 using the current time vector
+tsc1 = resample(tsc1, tsc1.Time);
 
+% Remove a ts from ts collection
+tsc1 = removets(tsc1, 'intersection3')
 
+tsc1.intersection2.DataInfo.Units = 'cars'
 
+% Set StartDate
+tsc1.TimeInfo.StartDate = 'DEC-25-2009 00:00:00';
 
+% specify time format
+tsc1.TimeInfo.Format = 'HH:MM';
 
-
+% Plot ts collection member
+figure; hold on;
+plot(tsc1.intersection1, '-b');
+plot(tsc1.intersection2, '.-m');
+hold off;
 
 
 
